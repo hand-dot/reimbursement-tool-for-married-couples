1af52720fe3b440de1a09104f0f292b82488ea16
Kyohei Fukuda <kyouhei.fukuda0729@gmail.com>
2023-03-25 18:08:16 +0900

    TMP

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,73 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+
+interface ExpenseDetail {
+  food: number;
+  communication: number;
+  water: number;
+  gasElectric: number;
+  rent: number;
+  education: number;
+}
+type Expense = {
+  month: string;
+  husband: ExpenseDetail;
+  wife: ExpenseDetail;
+};
 
 type ExpenseFormProps = {
   onSubmit: (expense: Expense) => void;
   month: string;
 };
 
-const ExpenseForm: React.FC<ExpenseFormProps> = ({ onSubmit, month }) => {
-  const [husbandExpenses, setHusbandExpenses] = useState({
-    food: 0,
-    communication: 0,
-    water: 0,
-    gasElectric: 0,
-    rent: 0,
-    education: 0,
-  });
+const getInitialExpenseDetail = (): ExpenseDetail => ({
+  food: 0,
+  communication: 0,
+  water: 0,
+  gasElectric: 0,
+  rent: 0,
+  education: 0,
+});
 
-  const [wifeExpenses, setWifeExpenses] = useState({
-    food: 0,
-    communication: 0,
-    water: 0,
-    gasElectric: 0,
-    rent: 0,
-    education: 0,
-  });
+const getCurrentMonth = () => {
+  const date = new Date();
+  return `${date.getFullYear()}-${("0" + (date.getMonth() + 1)).slice(-2)}`;
+};
+
+const getExpenseDetailSum = (expenseDetail: ExpenseDetail) =>
+  Object.values(expenseDetail).reduce((a, b) => a + b, 0);
+
+const renderExpenses = (
+  role: "husband" | "wife",
+  expenses: ExpenseDetail,
+  handleChange: (
+    event: React.ChangeEvent<HTMLInputElement>,
+    field: keyof ExpenseDetail
+  ) => void
+) => (
+  <div>
+    <h3>{role.charAt(0).toUpperCase() + role.slice(1)}</h3>
+    {Object.keys(expenses).map((key) => (
+      <div key={key}>
+        <label htmlFor={`${role}-${key}`}>{key}: </label>
+        <input
+          type="number"
+          id={`${role}-${key}`}
+          value={expenses[key as keyof typeof expenses]}
+          onChange={(event) =>
+            handleChange(event, key as keyof typeof expenses)
+          }
+        />
+      </div>
+    ))}
+  </div>
+);
+
+const ExpenseForm: React.FC<ExpenseFormProps> = ({ onSubmit, month }) => {
+  const [husbandExpenses, setHusbandExpenses] = useState(
+    getInitialExpenseDetail()
+  );
+
+  const [wifeExpenses, setWifeExpenses] = useState(getInitialExpenseDetail());
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -48,135 +93,29 @@ const ExpenseForm: React.FC<ExpenseFormProps> = ({ onSubmit, month }) => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <h2>Enter expenses for {month}</h2>
+      <h2 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+        Enter expenses for {month}
+      </h2>
       <div>
-        <h3>Husband</h3>
-        {Object.keys(husbandExpenses).map((key) => (
-          <div key={key}>
-            <label htmlFor={`husband-${key}`}>{key}: </label>
-            <input
-              type="number"
-              id={`husband-${key}`}
-              value={husbandExpenses[key as keyof typeof husbandExpenses]}
-              onChange={(event) =>
-                handleHusbandExpenseChange(
-                  event,
-                  key as keyof typeof husbandExpenses
-                )
-              }
-            />
-          </div>
-        ))}
-      </div>
-      <div>
-        <h3>Wife</h3>
-        {Object.keys(wifeExpenses).map((key) => (
-          <div key={key}>
-            <label htmlFor={`wife-${key}`}>{key}: </label>
-            <input
-              type="number"
-              id={`wife-${key}`}
-              value={wifeExpenses[key as keyof typeof wifeExpenses]}
-              onChange={(event) =>
-                handleWifeExpenseChange(event, key as keyof typeof wifeExpenses)
-              }
-            />
-          </div>
-        ))}
+        {renderExpenses("husband", husbandExpenses, handleHusbandExpenseChange)}
+        {renderExpenses("wife", wifeExpenses, handleWifeExpenseChange)}
       </div>
       <button type="submit">Submit Expenses</button>
     </form>
   );
 };
 
-type Expense = {
-  month: string;
-  husband: {
-    food: number;
-    communication: number;
-    water: number;
-    gasElectric: number;
-    rent: number;
-    education: number;
-  };
-  wife: {
-    food: number;
-    communication: number;
-    water: number;
-    gasElectric: number;
-    rent: number;
-    education: number;
-  };
-};
-
-const date = new Date();
-
-const ExpenseCalculator = () => {
-  const [expenses, setExpenses] = useState<Expense[]>([]);
-  const [month, setMonth] = useState(
-    `${date.getFullYear()}-${('0' + (date.getMonth() + 1)).slice(-2)}`
-  );
-
-  useEffect(() => {
-    // Load expenses from local storage or fetch from firestore in the future
-    const loadedExpenses = localStorage.getItem('expenses');
-    if (loadedExpenses) {
-      setExpenses(JSON.parse(loadedExpenses));
-    }
-    // Set current month
-    const date = new Date();
-    const currentMonth = `${date.getFullYear()}-${(
-      '0' +
-      (date.getMonth() + 1)
-    ).slice(-2)}`;
-    setMonth(currentMonth);
-  }, []);
-
-  useEffect(() => {
-    // Save expenses to local storage
-    localStorage.setItem('expenses', JSON.stringify(expenses));
-  }, [expenses]);
-
-  const addExpense = (newExpense: Expense) => {
-    setExpenses([...expenses, newExpense]);
-  };
-
-  const calculateReimbursement = (
-    husbandExpenses: number,
-    wifeExpenses: number
-  ) => {
-    return Math.abs(husbandExpenses - wifeExpenses);
-  };
-
-  const getMonthlyExpense = (expenses: Expense[], selectedMonth: string) => {
-    return expenses.find((expense) => expense.month === selectedMonth);
-  };
-
-  const [selectedMonth, setSelectedMonth] = useState(month);
-
-  const handleMonthSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setSelectedMonth(event.target.value);
-  };
-
-  const selectedExpense = getMonthlyExpense(expenses, selectedMonth);
-  const reimbursement = selectedExpense
-    ? calculateReimbursement(
-        Object.values(selectedExpense.husband).reduce((a, b) => a + b, 0),
-        Object.values(selectedExpense.wife).reduce((a, b) => a + b, 0)
-      )
-    : 0;
-
-  console.log(reimbursement);
-
-  // Render form and display components here
+const PastExpenses: React.FC<{
+  expenses: Expense[];
+  selectedMonth: string;
+  handleMonthSelect: (event: React.ChangeEvent<HTMLSelectElement>) => void;
+  selectedExpense: Expense | undefined;
+}> = ({ expenses, selectedMonth, handleMonthSelect, selectedExpense }) => {
   return (
-    <div>
-      {/* Render form to input expenses for husband and wife */}
-      <ExpenseForm onSubmit={addExpense} month={month} />
-      {/* Render list of months to select and view past expenses */}
+    <>
       <div>
         <label htmlFor="month-select">
-          Select month to view past expenses:{' '}
+          Select month to view past expenses:{" "}
         </label>
 
         <select
@@ -195,40 +134,107 @@ const ExpenseCalculator = () => {
       {selectedExpense && (
         <div>
           <h3>Expenses for {selectedExpense.month}</h3>
-          <h4>Husband</h4>
-          <ul>
-            {Object.entries(selectedExpense.husband).map(([key, value]) => (
-              <li key={key}>
-                {key}: {value}
-              </li>
-            ))}
-          </ul>
-          <h4>Wife</h4>
-          <ul>
-            {Object.entries(selectedExpense.wife).map(([key, value]) => (
-              <li key={key}>
-                {key}: {value}
-              </li>
-            ))}
-          </ul>
+          {[
+            ["Husband", selectedExpense.husband],
+            ["Wife", selectedExpense.wife],
+          ].map(([title, expense]) => (
+            <div>
+              <h4>{title as string}</h4>
+              <ul>
+                {Object.entries(expense).map(([key, value]) => (
+                  <li key={key}>
+                    {key}: {value}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
         </div>
       )}
-      {/* Display reimbursement calculation result */}
-      <div>
-        <h3>Reimbursement:</h3>
-        {reimbursement > 0
-          ? `To balance expenses, ${
-              Object.values(
-                selectedExpense ? selectedExpense.husband : {}
-              ).reduce((a, b) => a + b, 0) >
-              Object.values(selectedExpense ? selectedExpense.wife : {}).reduce(
-                (a, b) => a + b,
-                0
-              )
-                ? 'husband'
-                : 'wife'
-            } should give ¥${reimbursement} to the other.`
-          : 'Expenses are already balanced.'}
+    </>
+  );
+};
+
+const Reimbursement: React.FC<{ selectedExpense: Expense | undefined }> = ({
+  selectedExpense,
+}) => {
+  const calculateReimbursement = (
+    husbandExpenses: number,
+    wifeExpenses: number
+  ) => {
+    return Math.abs(husbandExpenses - wifeExpenses);
+  };
+
+  const reimbursement = selectedExpense
+    ? calculateReimbursement(
+        getExpenseDetailSum(selectedExpense.husband),
+        getExpenseDetailSum(selectedExpense.wife)
+      )
+    : 0;
+  return (
+    <div>
+      <h3>Reimbursement:</h3>
+      {selectedExpense && reimbursement > 0
+        ? `To balance expenses, ${
+            getExpenseDetailSum(selectedExpense.husband) >
+            getExpenseDetailSum(selectedExpense.wife)
+              ? "husband"
+              : "wife"
+          } should give ¥${reimbursement} to the other.`
+        : "Expenses are already balanced."}
+    </div>
+  );
+};
+
+const ExpenseCalculator = () => {
+  const [expenses, setExpenses] = useState<Expense[]>([]);
+  const [month, setMonth] = useState(getCurrentMonth());
+  const [selectedMonth, setSelectedMonth] = useState(month);
+
+  useEffect(() => {
+    // Load expenses from local storage or fetch from firestore in the future
+    const loadedExpenses = localStorage.getItem("expenses");
+    if (loadedExpenses) {
+      setExpenses(JSON.parse(loadedExpenses));
+    }
+    // Set current month
+    setMonth(getCurrentMonth());
+  }, []);
+
+  useEffect(() => {
+    // Save expenses to local storage
+    localStorage.setItem("expenses", JSON.stringify(expenses));
+  }, [expenses]);
+
+  const addExpense = (newExpense: Expense) => {
+    setExpenses([...expenses, newExpense]);
+  };
+
+  const getMonthlyExpense = (expenses: Expense[], selectedMonth: string) => {
+    return expenses.find((expense) => expense.month === selectedMonth);
+  };
+
+  const handleMonthSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedMonth(event.target.value);
+  };
+
+  const selectedExpense = getMonthlyExpense(expenses, selectedMonth);
+
+  // Render form and display components here
+  return (
+    <div className="bg-white py-24 px-6 sm:py-32 lg:px-8">
+      <div className="mx-auto max-w-2xl text-center">
+        {/* Render form to input expenses for husband and wife */}
+        <ExpenseForm onSubmit={addExpense} month={month} />
+        {/* Render list of months to select and view past expenses */}
+        <PastExpenses
+          expenses={expenses}
+          selectedMonth={selectedMonth}
+          handleMonthSelect={handleMonthSelect}
+          selectedExpense={selectedExpense}
+        />
+        {/* Display reimbursement calculation result */}
+        <Reimbursement selectedExpense={selectedExpense} />
       </div>
     </div>
   );
